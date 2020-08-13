e8ecaf8079aab5df30dad9e8b6a549b33c08aeaf
ClarkTheSup <lknkoqq123@163.com>
2020-08-13 17:42:23 +0800

    build project

---
+++ src/main/java/com/thoughtwokrs/quizbackend/QuizBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtwokrs.quizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thoughtwokrs/quizbackend/QuizBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtwokrs.quizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
